48a15da796205962d0ddca7a5a31f089d8e90d79
Akanksha0619 <maratheaakanksha91@gmail.com>
2024-04-28 01:05:07 +0530

    add functions

--- Mental_Health/urls.py
+++ Mental_Health/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
-
+from .views import *
 urlpatterns = [
-    path('', views.home, name='home'),
+  
+  path('home/', home, name="home"),
+  path('register/',register_user,name="register"),
+  path('', login_user, name="login"),
+  path('logout/',logout_user,name="logout"),
+
+  
 ]
--- Mental_Health/views.py
+++ Mental_Health/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import  CreateUserForm
 
 def home(request):
     return render(request, 'home.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect('home')  # Redirect to the home page after successful registration
+    else:
+        form = CreateUserForm()
+    return render(request, 'register_page.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, f'Welcome back, {username}! You are now logged in.')
+            return redirect('home')  # Adjust the URL name to your home page
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+            return redirect('login')
+
+    return render(request, 'login_page.html')
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('login')

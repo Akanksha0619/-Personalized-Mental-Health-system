75e8123d2db0449fa7f21aa6ff7a79f08a3fb70b
Muskanrath2274 <rathoremuslan597@gmail.com>
2024-04-28 05:52:33 +0530

    commit

--- Mental_Health/views.py
+++ Mental_Health/views.py
@@ -1,42 +1,107 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .forms import  CreateUserForm
+from django.shortcuts import render , redirect
 
-def home(request):
-    return render(request, 'home.html')
+from django.contrib.auth.models import User
 
-def register_user(request):
+from .models import Mobile_Varification
+import random
+
+import http.client
+
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+
+def send_otp(mobile , otp):
+    print("FUNCTION CALLED")
+    conn = http.client.HTTPSConnection("api.msg91.com")
+    authkey = settings.AUTH_KEY 
+    headers = { 'content-type': "application/json" }
+    url = "http://control.msg91.com/api/sendotp.php?otp="+otp+"&message="+"Your otp is "+otp +"&mobile="+mobile+"&authkey="+authkey+"&country=91"
+    conn.request("GET", url , headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data)
+    return None
+
+
+
+def login_attempt(request):
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Automatically log in the user after registration
-            return redirect('home')  # Redirect to the home page after successful registration
-    else:
-        form = CreateUserForm()
-    return render(request, 'register_page.html', {'form': form})
+        mobile = request.POST.get('mobile')
+        
+        user = Mobile_Varification.objects.filter(mobile = mobile).first()
+        
+        if user is None:
+            context = {'message' : 'User not found' , 'class' : 'danger' }
+            return render(request,'login_page.html' , context)
+        
+        otp = str(random.randint(1000 , 9999))
+        user.otp = otp
+        user.save()
+        send_otp(mobile , otp)
+        request.session['mobile'] = mobile
+        return redirect('login_otp')        
+    return render(request,'login_page.html')
 
-def login_user(request):
+
+def login_otp(request):
+    mobile = request.session['mobile']
+    context = {'mobile':mobile}
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.info(request, f'Welcome back, {username}! You are now logged in.')
-            return redirect('home')  # Adjust the URL name to your home page
+        otp = request.POST.get('otp')
+        profile = Mobile_Varification.objects.filter(mobile=mobile).first()
+        
+        if otp == profile.otp:
+            user = User.objects.get(id = profile.user.id)
+            login(request , user)
+            return redirect('cart')
         else:
-            messages.error(request, 'Invalid username or password. Please try again.')
-            return redirect('login')
+            context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
+            return render(request,'login_otp.html' , context)
+    
+    return render(request,'login_otp.html' , context)
+    
+    
 
-    return render(request, 'login_page.html')
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        
+        check_user = User.objects.filter(email = email).first()
+        check_profile = Mobile_Varification.objects.filter(mobile = mobile).first()
+        
+        if check_user or check_profile:
+            context = {'message' : 'User already exists' , 'class' : 'danger' }
+            return render(request,'register.html' , context)
+            
+        user = User(email = email , first_name = name)
+        user.save()
+        otp = str(random.randint(1000 , 9999))
+        profile = Mobile_Varification(user = user , mobile=mobile , otp = otp) 
+        profile.save()
+        send_otp(mobile, otp)
+        request.session['mobile'] = mobile
+        return redirect('otp')
+    return render(request,'register.html')
 
-def logout_user(request):
-    logout(request)
-    messages.info(request, "You have successfully logged out.")
-    return redirect('login')
+def otp(request):
+    mobile = request.session['mobile']
+    context = {'mobile':mobile}
+    if request.method == 'POST':
+        otp = request.POST.get('otp')
+        profile = Mobile_Varification.objects.filter(mobile=mobile).first()
+        
+        if otp == profile.otp:
+            return redirect('cart')
+        else:
+            print('Wrong')
+            
+            context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
+            return render(request,'otp.html' , context)
+            
+        
+    return render(request,'otp.html' , context)
